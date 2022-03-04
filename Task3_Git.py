6d6b14e8205206a9f37682829693c003b53b8f0f
Rajvi Zala <rajvisinhzala@gmail.com>
2022-03-04 22:43:42 +0530

    I have completed Task 3

---
+++ Task3_Git.py
@@ -0,0 +1,11 @@
+#Made By Rajvi Zala for Task 3
+
+
+# This function is for reversing the string.
+def reverse_string(str):
+    return str[::-1]
+
+
+str="iamstudyinginsou"
+str2=reverse_string(str)
+print(str2)
